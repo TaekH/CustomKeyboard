95438114993f12458051523aa57da389d5da9a2b
Taekhwan Han <hth3396@gmail.com>
2023-07-13 03:11:01 +0900

    feat: #1 setUpRowView() 구현 및  setUpRowStackView() 수정 StackView로는 키보드의 두번째 Row UI를 구현하기에 한계가 있어 setUpRowView()를 따로 생성하여 두번째 Row UI를 구현합니다

--- CustomKeyboard/KoreanKeyboard/View/KeyboardView.swift
+++ CustomKeyboard/KoreanKeyboard/View/KeyboardView.swift
@@ -8,30 +8,17 @@
 import UIKit
 
 class KeyboardView: UIView {
-
+    
     lazy var keyboardStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .vertical
-        stackView.spacing = Size.keyboardRowSpacing(CGFloat(keys.count))
+        stackView.spacing = 0
         stackView.backgroundColor = .darkGray
         stackView.layoutMargins = Size.keyboardEdgeInsets()
         stackView.isLayoutMarginsRelativeArrangement = true
         stackView.distribution = .fillEqually
         return stackView
     }()
-    
-//    let keyButton: (String) -> UIButton = { title in
-//        let button = UIButton(type: .system)
-//        button.setTitle(title, for: .normal)
-//        button.titleLabel?.adjustsFontSizeToFitWidth = true
-//        button.titleLabel?.font = UIFont.systemFont(ofSize: 20)
-//        button.backgroundColor = .systemGray
-//        button.setTitleColor(.white, for: .normal)
-//        button.layer.cornerRadius = Size.keyRadius
-//        button.widthAnchor.constraint(equalToConstant: Size.keyWidth).isActive = true
-//        button.heightAnchor.constraint(equalToConstant: Size.keyHeight).isActive = true
-//        return button
-//    }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -44,24 +31,61 @@ class KeyboardView: UIView {
     }
     
     private func setUpRowStackView(keyRow: [KeyModel]) -> UIStackView {
-         let rowStackView = UIStackView()
-         rowStackView.axis = .horizontal
-        rowStackView.spacing = Size.keyboardItemSpacing(Size.keyWidth, CGFloat(keys.first?.count ?? 10))
-         rowStackView.distribution = .fillEqually
-        rowStackView.alignment = .center
-         
+        let rowStackView = UIStackView()
+        rowStackView.axis = .horizontal
+        rowStackView.spacing = 0
+        rowStackView.distribution = keyRow.count <= 4 ? .fillProportionally : .equalSpacing
+        rowStackView.alignment = .fill
+        
+        let paddingView = UIView()
+        paddingView.widthAnchor.constraint(equalToConstant: (Size.width - Size.keyWidth * CGFloat(keyRow.count)) / 2).isActive = true
+        
         keyRow.forEach { key in
-            let button = KeyButton(key.keyword)
+            let button = KeyButtonView(key.keyword)
+            if key.uniValue < 100{
+                button.widthAnchor.constraint(equalToConstant: Size.keyWidth).isActive = true
+                button.heightAnchor.constraint(equalToConstant: Size.keyWidth * 1.35).isActive = true
+            }
             rowStackView.addArrangedSubview(button)
         }
-         
-         return rowStackView
-     }
+        
+        return rowStackView
+    }
+    
+    private func setUpRowView(keyRow: [KeyModel]) -> UIView {
+        let keyboardRowView = UIView()
+        let leadingSpacing = (Size.width - Size.keyWidth * CGFloat(keyRow.count)) / 2
+        keyboardRowView.widthAnchor.constraint(equalToConstant: Size.width).isActive = true
+        
+        var firstKeyView: KeyButtonView?
+        keyRow.forEach { key in
+            let keyButtonView = KeyButtonView(key.keyword)
+            keyboardRowView.addSubview(keyButtonView)
+            keyButtonView.translatesAutoresizingMaskIntoConstraints = false
+            
+            if let firstKeyView = firstKeyView {
+                keyButtonView.leadingAnchor.constraint(equalTo: firstKeyView.trailingAnchor).isActive = true
+            } else {
+                keyButtonView.leadingAnchor.constraint(equalTo: keyboardRowView.leadingAnchor, constant: leadingSpacing).isActive = true
+            }
+            keyButtonView.topAnchor.constraint(equalTo: keyboardRowView.topAnchor).isActive = true
+            keyButtonView.bottomAnchor.constraint(equalTo: keyboardRowView.bottomAnchor).isActive = true
+            
+            keyButtonView.widthAnchor.constraint(equalToConstant: Size.keyWidth).isActive = true
+            keyButtonView.heightAnchor.constraint(equalToConstant: Size.keyWidth * 1.35).isActive = true
+            
+            firstKeyView = keyButtonView
+        }
+        return keyboardRowView
+    }
+    
     
     private func setUpKeyboardStackView() {
         for row in keys {
-            let rowStackView = setUpRowStackView(keyRow: row)
-            keyboardStackView.addArrangedSubview(rowStackView)
+            let rowView = row.contains { key in
+                key.keyword == "ㅁ"
+            } ? setUpRowView(keyRow: row) : setUpRowStackView(keyRow: row)
+            keyboardStackView.addArrangedSubview(rowView)
         }
     }
 }
