309b1cbbaa27e66e2780dfca3cda4c18dd641721
Taekhwan Han <hth3396@gmail.com>
2023-07-12 14:31:41 +0900

    feat: #1 KeyboardStackView 생성 바탕이 될 StackView 생성

--- CustomKeyboard/KoreanKeyboard/View/KeyboardView.swift
+++ CustomKeyboard/KoreanKeyboard/View/KeyboardView.swift
@@ -8,5 +8,31 @@
 import UIKit
 
 class KeyboardView: UIView {
+    //MARK: 현재 뷰 크기 반환 계산 프로퍼티
+    var height: CGFloat {
+        return frame.size.height
+    }
     
+    var width: CGFloat {
+        return frame.size.width
+    }
+    
+    lazy var keyboardStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = Size.keyboardRowSpacing(height, Size.keySize(width).1)
+        stackView.backgroundColor = .darkGray
+        stackView.layoutMargins = Size.keyboardEdgeInsets()
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.distribution = .fillEqually
+        return stackView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
