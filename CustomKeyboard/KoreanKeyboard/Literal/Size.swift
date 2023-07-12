a563d81fd465b784ce8850d1d17b9e5c759fd32c
Taekhwan Han <hth3396@gmail.com>
2023-07-12 14:16:49 +0900

    feat: #1 키보드 세부의 Spacing이나 크기를 지정할 수 있는 Literal 추가 지정된 값을 전달함에 따라서 키보드의 레이아웃 크기를 지정할 수 있는 Literal을 구현합니다

---
+++ CustomKeyboard/KoreanKeyboard/Literal/Size.swift
@@ -0,0 +1,23 @@
+//
+//  Size.swift
+//  KoreanKeyboard
+//
+//  Created by 한택환 on 2023/07/12.
+//
+
+import UIKit
+
+enum Size {
+    
+    static func keyboardRowSpacing(_ keyboardHeight: CGFloat, _ keyHeight: CGFloat) -> CGFloat {
+        return (keyboardHeight - keyHeight * 4) / 3
+    }
+    
+    static func keyboardItemSpacing(_ keyboardWidth: CGFloat, _ keyWidth: CGFloat, _ keyCount: Int) -> CGFloat {
+        return (keyboardWidth - (keyWidth * CGFloat(keyCount))) / CGFloat((keyCount - 1))
+    }
+    
+    static func keyboardEdgeInsets() -> UIEdgeInsets {
+        return UIEdgeInsets(top: 8, left: 3, bottom: 8, right: 3)
+    }
+}
