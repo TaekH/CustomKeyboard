4798f54f8d84b4a7b7b46cf75f631da1c00a591a
Taekhwan Han <hth3396@gmail.com>
2023-07-15 01:16:16 +0900

    feat: #10 키보드 입력 시 한글을 조합해서 입력하는 기능 구현

--- CustomKeyboard/KoreanKeyboard/Controller/KeyboardViewController.swift
+++ CustomKeyboard/KoreanKeyboard/Controller/KeyboardViewController.swift
@@ -85,6 +85,7 @@ extension KeyboardViewController: KeyboardViewDelegate {
             inputState = 0
         default:
             handleKeyInput(key)
+            print("case : ", inputState, "버퍼 : ", buffer)
             if shiftKeyState == .constant {
                 shiftKeyState = .normal
                 resetKeyboardView()
@@ -156,7 +157,7 @@ private extension KeyboardViewController {
 }
 
 private extension KeyboardViewController {
-
+    
     func makeWord(_ cho: Int, _ jung: Int, _ jong: Int) -> String {
         guard let result = UnicodeScalar(0xAC00 + 28 * 21 * cho + 28 * jung  + jong) else { return "오류가 발생했습니다." }
         return String(Character(result))
@@ -169,23 +170,39 @@ private extension KeyboardViewController {
         }
         
         switch inputState {
-        case 0:
+        case 0: // 중성만 입력하면 다시 중성 입력할 수도 있는 부분
             if Hangul.jungs.contains(key.keyword) {
-                textDocumentProxy.insertText(key.keyword)
+                if !buffer.isEmpty {
+                    let doubleJung = Hangul.makeJungDoublePhoneme(buffer.last!, key).keyword
+                    if doubleJung != key.keyword {
+                        textDocumentProxy.deleteBackward()
+                        textDocumentProxy.insertText(doubleJung)
+                        buffer.removeAll()
+                    }
+                    else {
+                        textDocumentProxy.insertText(key.keyword)
+                        buffer.removeAll()
+                        buffer.append(key)
+                    }
+                } else {
+                    textDocumentProxy.insertText(key.keyword)
+                    buffer.append(key)
+                }
             } else {
                 textDocumentProxy.insertText(key.keyword)
                 buffer.append(key)
                 inputState = 1
             }
-        case 1:
+        case 1: // 정상이라면 중성을 입력하는 부분
             if Hangul.jungs.contains(key.keyword) {
                 guard let cho = buffer.last else { return }
                 if buffer.count == 1 {
                     textDocumentProxy.deleteBackward()
-                } else {
+                } else { // DoubleJong으로 이루어진 것
                     textDocumentProxy.deleteBackward()
-                    let chojungjong = Array(buffer.suffix(3))
-                    textDocumentProxy.insertText(makeWord(chojungjong[0].uniValue, chojungjong[1].uniValue, 0))
+                    let chojungjong = Array(buffer.suffix(4))
+                    let jong = Hangul.makeJongDoublePhoneme(chojungjong[2], KeyModel(keyword: "", uniValue: 0))
+                    textDocumentProxy.insertText(makeWord(chojungjong[0].uniValue, chojungjong[1].uniValue, jong.uniValue))
                 }
                 textDocumentProxy.insertText(makeWord(cho.uniValue, key.uniValue, 0))
                 buffer.append(key)
@@ -196,14 +213,23 @@ private extension KeyboardViewController {
                 buffer.append(key)
             }
         case 2:
+            let chojung = Array(buffer.suffix(2))
             if Hangul.jungs.contains(key.keyword) {
-                buffer.removeAll()
-                textDocumentProxy.insertText(key.keyword)
-                inputState = 0
+                //doubleJung을 확인
+                let doubleJung = Hangul.makeJungDoublePhoneme(chojung[1], key)
+                if doubleJung.keyword != key.keyword {
+                    textDocumentProxy.deleteBackward()
+                    textDocumentProxy.insertText(makeWord(chojung[0].uniValue, doubleJung.uniValue, 0))
+                    buffer.removeLast()
+                    buffer.append(doubleJung)
+                } else {
+                    buffer.removeAll()
+                    textDocumentProxy.insertText(key.keyword)
+                    inputState = 0
+                }
             } else {
-                let chojung = Array(buffer.suffix(2))
                 textDocumentProxy.deleteBackward()
-                textDocumentProxy.insertText(makeWord(chojung[0].uniValue, chojung[1].uniValue, Hangul.makeDoublePhoneme(key, KeyModel(keyword: "", uniValue: 0)).uniValue))
+                textDocumentProxy.insertText(makeWord(chojung[0].uniValue, chojung[1].uniValue, Hangul.makeJongDoublePhoneme(key, KeyModel(keyword: "", uniValue: 0)).uniValue))
                 if Hangul.jongs.contains(key.keyword) {
                     buffer.append(key)
                     inputState = 3
@@ -214,12 +240,13 @@ private extension KeyboardViewController {
             }
         case 3:
             let chojungjong = Array(buffer.suffix(3))
-            let doubleJong = Hangul.makeDoublePhoneme(chojungjong[2], key)
+            let doubleJong = Hangul.makeJongDoublePhoneme(chojungjong[2], key)
             print(doubleJong.keyword)
+            //doublejong이 참인경우
             if doubleJong.keyword != "" {
                 textDocumentProxy.deleteBackward()
                 textDocumentProxy.insertText(makeWord(chojungjong[0].uniValue, chojungjong[1].uniValue, doubleJong.uniValue))
-                buffer.removeAll()
+                //buffer.removeAll()
                 buffer.append(key)
                 inputState = 1
             }
@@ -231,7 +258,7 @@ private extension KeyboardViewController {
             } else if Hangul.jungs.contains(key.keyword) {
                 guard let cho = buffer.last else { return }
                 if buffer.count == 1 {
-                    textDocumentProxy.deleteBackward()
+                    //textDocumentProxy.deleteBackward()
                 } else {
                     textDocumentProxy.deleteBackward()
                     let chojungjong = Array(buffer.suffix(3))
@@ -245,9 +272,5 @@ private extension KeyboardViewController {
         default:
             break
         }
-    }
-    
-    func getDoubleJong(_ first: KeyModel, _ last: KeyModel) -> KeyModel {
-        return first
     }
 }
